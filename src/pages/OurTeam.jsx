ab5c7169c2298f2ba3c23d3e1a983541521b0b4d
rnrgll <shuajjjjj@naver.com>
2024-01-18 20:41:28 +0900

    OurVision, OurTeam, Contact 페이지 merge (image 오류 있음)

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,8 +1,40 @@
-function OurTeam() {
+import React from "react";
+export default function OurTeam() {
   return (
-    <div style={{ marginTop: "100px" }}>
-      <h1>Our Team page</h1>
+    <div className="team-container">
+      <div className="team-wrap">
+        <div className="team-header">Our Team</div>
+        <div className="team-main">
+          <div className="team-position1">
+            plan Design
+            <div className="photo">
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+            </div>
+            이름/학교 이름/학교
+          </div>
+          <div className="team-position2">
+            Front-end
+            <div className="photo">
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+            </div>
+            이름/학교 이름/학교 이름/학교 이름/학교
+          </div>
+          <div className="team-position2">
+            Back-end
+            <div className="photo">
+              <img src="../assets/logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+              <img src="logo.svg" alt="로고" />
+            </div>
+            이름/학교 이름/학교 이름/학교 이름/학교
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-export default OurTeam;
