4c1d809461450f95698955d38411b9711edd110f
rnrgll <shuajjjjj@naver.com>
2024-01-20 16:08:13 +0900

    Splash Animation 작업 중

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,12 +1,25 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 import { ReactComponent as LogoSvg } from "../assets/images/logo.svg";
-
+import { motion } from "framer-motion";
+//---animation & transition---//
+//fade out
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    filter: blur(0);
+  }
+  to {
+    opacity: 0;
+    filter: blur(10px);
+  }
+`;
+//---component---//
 const Wrapper = styled.div`
   width: 100%;
+  height: 100%;
   background-color: #f9f8f8;
-  height: 90vh;
   position: relative;
   overflow: hidden;
 `;
@@ -17,6 +30,7 @@ const Logo = styled(LogoSvg)`
   position: absolute;
   bottom: 37px;
   right: 36px;
+  animation: ${fadeOut} 1s ease-in-out;
 `;
 
 //background green box 공통 스타일 (BGBox first child를 기본으로 설정)
@@ -24,7 +38,7 @@ const commonBGBox = css`
   width: 200%;
   height: 36.07px;
   left: -79.61px;
-  top: 1006.91px;
+  top: 900px;
   position: absolute;
   transform: rotate(-30deg);
   transform-origin: 0 0;
@@ -35,39 +49,62 @@ const BgMainBox = styled.div`
   ${commonBGBox}
   height: 197.6px;
   left: -160px;
-  top: 774.78px;
+  top: 600px;
+  padding-left: 300px;
+  padding-top: 10px; //글자 세로 방향 중앙 정렬을 위해 조정(font 특성상 align-item center 효과 없음)
+`;
+const MainTitle = styled(motion.div)`
   font-size: 180px;
   font-family: "Knewave", "SUIT-Regular";
   font-weight: 400;
-  display: flex;
-  align-items: center;
-  padding-left: 300px;
+  color: rgba(16, 124, 65, 0.79);
 `;
 
-const BGBox = styled.div`
+const BGBox = styled(motion.div)`
   ${commonBGBox}
   &:nth-child(2) {
     left: -7.61px;
-    top: 1037.91px;
+    top: 790px;
   }
 `;
+
 function Splash() {
   const navigate = useNavigate();
-  useEffect(() => {
-    console.log("useEffect 실행");
-    const timer = setTimeout(() => {
-      navigate("/home");
-    }, 2000); //2초
+  // useEffect(() => {
+  //   console.log("useEffect 실행");
+  //   const timer = setTimeout(() => {
+  //     navigate("/home");
+  //   }, 2000); //2초
 
-    return () => clearTimeout(timer);
-  }, [navigate]);
+  //   return () => clearTimeout(timer);
+  // }, [navigate]);
 
   return (
     <Wrapper>
-      <BgMainBox>Easy Excel</BgMainBox>
-      <BGBox />
-      <BGBox />
-      <Logo />
+      <BgMainBox>
+        <MainTitle
+          initial={{ opacity: 1, x: 0 }}
+          animate={{ opacity: 0, x: -1000 }}
+          transition={{ duration: 1 }}
+        >
+          EASY EXCEL
+        </MainTitle>
+      </BgMainBox>
+      <BGBox
+        initial={{ opacity: 1, x: 0 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      />
+      <BGBox
+      // initial={{ opacity: 1, x: 0 }}
+      // animate={{ opacity: 1, x: 0 }}
+      // transition={{ duration: 1 }}
+      />
+      <Logo
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+      />
     </Wrapper>
   );
 }
