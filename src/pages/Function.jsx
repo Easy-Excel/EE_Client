26ec4f196f7da1742ace93c93e930feb96e7ac72
jinjukim_web <63224644+jinjukimink@users.noreply.github.com>
2024-01-22 21:39:03 +0900

    Home2추가
    Home에서 로고없는 버전인 Home2추가

--- src/pages/Function.jsx
+++ src/pages/Function.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Home from "../pages/Home";
+//import Home from "../pages/Home";
 import ButtonA from "../components/ButtonA";
-
+import Home2 from "../pages/Home2";
 //함수 페이지//로 가야함
 function Function(){
     return(
     <>
-    <Home/>
+    <Home2/>
     <ButtonA/>
     </>
     );
---
+++ src/pages/Home2.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import Logo from "../assets/images/Logo.svg";
+import Navbar from "../components/Navbar";
+import ButtonA from "../components/ButtonA";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #CEEBDE;
+  height: calc(100vh - 88px);
+`;
+const ServContainer = styled.div`
+  background-color: #f9f8f8;
+  height: 80px;
+  text-align: center;
+`;
+
+const Content = styled.div`
+  //width: calc(100% - 200px);
+  width: 80%;//백분율로 조장해야 화면 크기에 따라 조절됨
+  height: 700px;
+  background-color: white;
+  margin: auto;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  margin-top:0px;
+`;
+const LogoImg = styled.img`
+  width: 250px;
+  height: 300px;
+  margin: auto;
+  background-size: cover;
+  position: absolute;
+  top: 140px;
+`;
+const Footer =styled.footer`
+  position: absolute;
+  color: #107c41;
+  font-weight: 400;
+  font-size: 10px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  bottom: 3px;
+`
+
+export default function Home2() {
+  return (
+    <MainContainer>
+      <ServContainer>서비스 상세 내용 </ServContainer>
+      <Navbar />
+      <Content>
+      </Content>
+      <Footer>Copyright © Easy Excel</Footer>
+    </MainContainer>
+    
+  );
+}
+
