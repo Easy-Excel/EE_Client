1425b2f59aedd4c04741340aa4e48064f78fd681
Seo yeon <mariajang16@ewhain.net>
2024-01-26 16:12:45 +0900

    단축키 표

--- src/pages/Shortcut.jsx
+++ src/pages/Shortcut.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ButtonS from "../components/ButtonS";
+import Keytable from "../components/Keytable";
 //함수 페이지//로 가야함
 function Shortcut() {
   return (
     <div>
       <ButtonS />
+      <Keytable/>
     </div>
   );
 }
