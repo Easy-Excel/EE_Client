6055ad3a2f3bb2aa3fc8006852cfa97f19c64cf0
rnrgll <shuajjjjj@naver.com>
2024-02-10 01:03:49 +0900

    Fix: 문자별 함수 조회 props 오류 해결

--- src/components/contents/functionPage/QtoZ.jsx
+++ src/components/contents/functionPage/QtoZ.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Container from "./FuncListContainer";
 
-export default function QtoZ({ functionsList, sortingType }) {
+export default function QtoZ({ functionsList, sortingType, content }) {
   return (
     <Container
       funcList={functionsList}
       listSize={functionsList.length}
       sortingType={sortingType}
+      content={content}
     />
   );
 }
